0b49abeea929fd8312999b3f199c40e1acf402e1
Amine BENLAMA <benlama.amine@gmail.com>
2023-11-25 16:28:53 +0100

    implementing customer component and its childs

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cm-orders',
+  templateUrl: './customer.component.html'
+})
+export class CustomerComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {   }
+
+}
